b28c8546f95350899f0edfd267c9015179db901f
Patrick Tschackert <ptschack@gmail.com>
2019-03-17 21:05:05 +0100

    some fixes

--- run_playbooks.sh
+++ run_playbooks.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #sudo dnf install python ansible openssh
 #sudo systemctl start sshd
-ansible-playbook -i hosts site.yml --ask-become-pass
+if [ ! -z "$1" ]; then
+    ansible-playbook -i hosts site.yml --ask-become-pass --limit $1
+else
+    ansible-playbook -i hosts site.yml --ask-become-pass
+fi
